8f9443f3c3163d0aade22effd3251e31577dfe02
Horther <77111164+Horther@users.noreply.github.com>
2025-08-25 11:55:00 +0100

    same

--- calculadora_aritimetica/src/App.vue
+++ calculadora_aritimetica/src/App.vue
@@ -41,6 +41,8 @@
       <option value="sub">Subtração</option>
       <option value="mult">Multiplicação</option>
       <option value="div">Divisão</option>
+      <option value="pow">Potencia</option>
+      <option value="raiz">Raiz Quadrada</option>
     </select>
     <div class="pt-3">
       <input class="me-3" type="number" v-model.number="estado.numero1" @keyup="calcula()">
